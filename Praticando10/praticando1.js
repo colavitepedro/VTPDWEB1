74cf7939b7f517d696f03a875e6c99d4c4d8d5ed
colavitepedro <colavitepedro1@gmail.com>
2025-11-06 09:20:23 -0300

    Adicionando Praticando 10 - JavaScript

---
+++ Praticando10/praticando1.js
@@ -0,0 +1,12 @@
+let nome = prompt("Qual é o seu nome?");
+if (nome) {
+  alert("Olá, " + nome + "! Seja bem-vindo(a)!");
+  let sabeProgramar = confirm("Você sabe programar em JavaScript?");
+  if (sabeProgramar) {
+    alert("Que ótimo " + nome + ", espero que você aprenda ainda mais!");
+  } else {
+    alert("Não tem problema " + nome + ", você irá aprender agora!");
+  }
+} else {
+  alert("Você cancelou o prompt");
+}

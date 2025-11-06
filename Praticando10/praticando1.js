a724f07521026342b59339c1f4fda3907a2fa170
colavitepedro <colavitepedro1@gmail.com>
2025-11-06 09:22:37 -0300

    Formatando

--- Praticando10/praticando1.js
+++ Praticando10/praticando1.js
@@ -1,12 +1,13 @@
 let nome = prompt("Qual é o seu nome?");
+
 if (nome) {
   alert("Olá, " + nome + "! Seja bem-vindo(a)!");
   let sabeProgramar = confirm("Você sabe programar em JavaScript?");
-  if (sabeProgramar) {
-    alert("Que ótimo " + nome + ", espero que você aprenda ainda mais!");
-  } else {
-    alert("Não tem problema " + nome + ", você irá aprender agora!");
-  }
+    if (sabeProgramar) {
+      alert("Que ótimo " + nome + ", espero que você aprenda ainda mais!");
+    } else {
+      alert("Não tem problema " + nome + ", você irá aprender agora!");
+    }
 } else {
   alert("Você cancelou o prompt");
 }
